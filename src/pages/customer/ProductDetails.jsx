a37452f3f15c4017abb0722fb3d8885e26315571
Moka Mani Teja <maniteja517@gmail.com>
2026-09-18 18:59:20 +0530

    Fix deployed product details API URL

--- src/pages/customer/ProductDetails.jsx
+++ src/pages/customer/ProductDetails.jsx
@@ -19,7 +19,7 @@ import { useCart } from "../../context/CartContext";
 
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/products";
+const API_URL = "/api/products";
 
 
 function ProductDetails() {
